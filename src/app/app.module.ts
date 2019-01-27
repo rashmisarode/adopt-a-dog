747fa82cbb53901b0f4875e4362b3426ab01ed90
rashmisarode <rashmi.sarode@hal.hitachi.com>
2019-01-26 22:01:00 -0800

    service created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DogListingComponent } from './dog-listing/dog-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DogListingComponent
   ],
   imports: [
     BrowserModule
